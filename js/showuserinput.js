d7e67e7b01fe31858c82cc18bd29aeb4bcbaf541
Jean Borger <jeanmarieborger@gmail.com>
2015-11-12 13:45:45 -0500

    user text input is now appended to image user selected

--- js/showuserinput.js
+++ js/showuserinput.js
@@ -1,28 +1,28 @@
-//Goal: Ask users to select an image and write brief text; Output image and user text to page
+//Goal: Ask users to select an image and write brief text; output image and user text to page.  
 
-//Steps:  
-
-//Collect image selection and store it in a variable
-
-var imageInput; //image selected by user
-var imageToDisplay; //image to display to page
+//When image is clicked, display that image to page and hide gallery of images.
+	//Bind event handler to click on image. Get "src" attribute of clicked image.
+	//Hide gallery of images, and create new image element to replace it on page.
+	//Set its width and height and set its "src" attribute to that of selected image.
+	//Hide gallery and append new image element to div with id "picturebox."
 
 $().ready(function () {
 	$("img").click(function () {
 		var imgName = $(this).attr("src");
-		
 		$("#gallery").css("display","none");
 		var img = document.createElement("img");
-		img.height = "500px";
-		img.width = "100%";		
+		img.style.width = "100%";
+		img.style.height = "auto";
 		img.setAttribute("src", imgName);
-		$("#picturebox").append(img);    
+		$("#picturebox").append(img);  
 	});	
 });
 
 
-
-//Ask for text input and bind to button click, store value of input in a variable (twitchToDisplay)
+//On button click, collect user input and output it to the page.
+	//Store value of text entered by user in a variable.
+	//Bind event handler to click on button.
+	//Display text to page.
 
 var twitchInput = document.getElementById("twitch-input"); //text entered by user
 var addButton = document.getElementsByTagName("button")[0]; //first button
@@ -31,8 +31,9 @@ var twitchToDisplay; //user text to display to page
 addButton.onclick = function () {
 	twitchToDisplay = twitchInput.value;
 	console.log(twitchToDisplay);
+//	document.getElementById("user-text").innerHTML = twitchToDisplay;
 	twitchInput.value = "";
-	console.log(twitchInput.value);
+	$("#picturebox").append(twitchToDisplay);  
 };
 
 
