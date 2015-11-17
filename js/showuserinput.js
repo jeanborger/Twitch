a76c25e3490dd5da72f1a9efa1665a652f41758b
Jean Borger <jeanmarieborger@gmail.com>
2015-11-17 18:14:30 -0500

    created div for final output from user; formatted subpage headers; corrected spacing issue

--- js/showuserinput.js
+++ js/showuserinput.js
@@ -1,14 +1,16 @@
-//Goal: Ask users to select an image and write brief text; output image and user text to page.  
+//Goal: Ask users to select an image and write brief text; output image overlaid with user text to page.  
 
-//When image is clicked, display that image to page and hide gallery of images.
+//When image is clicked, display that image to page and hide gallery of images; save "src" attribute of image.
 	//Bind event handler to click on image. Get "src" attribute of clicked image.
 	//Hide gallery of images, and create new image element to replace it on page.
 	//Set its width and height and set its "src" attribute to that of selected image.
 	//Hide gallery and append new image element to div with id "picturebox."
 
+var imgName;
+
 $().ready(function () {
 	$("img").click(function () {
-		var imgName = $(this).attr("src");
+		imgName = $(this).attr("src");
 		$("#gallery").css("display","none");
 		$("#first-instruction").css("display","none");
 		var img = document.createElement("img");
@@ -19,23 +21,26 @@ $().ready(function () {
 	});	
 });
 
-
-//On button click, collect user input and output it to the page.
-	//Store value of text entered by user in a variable.
+//When twitch button is clicked, collect user input and store to variable. Then output selected image with text overlaid on top.
 	//Bind event handler to click on button.
-	//Display text to page.
+	//Store value of text entered by user in a variable.
+	//Create new div to hold selected image as background image and user text as h1 element.
 
 var twitchInput = document.getElementById("twitch-input"); //text entered by user
 var addButton = document.getElementsByTagName("button")[0]; //first button
 var twitchToDisplay; //user text to display to page
 
 addButton.onclick = function () {
+//collect value of user input and log to console to test
+//set value back to empty string
 	twitchToDisplay = twitchInput.value;
 	console.log(twitchToDisplay);
-//	document.getElementById("user-text").innerHTML = twitchToDisplay;
 	twitchInput.value = "";
-	$("#picturebox").append(twitchToDisplay);  
+	document.getElementById("input-display").innerHTML = twitchToDisplay;
+
+//	$("#picturebox").append(twitchToDisplay);  
 };
+
 
 
 //Set image as background to an HTML element (div with id "display-input")
